db1dc3e7e776aa63c774808c7014398c1cb3aa7f
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2024-10-25 19:10:36 +0530

    routing implemanted

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {createBrowserRouter,RouterProvider} from "react-router-dom"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <h1>hello</h1>);
+const router =createBrowserRouter([
+  {
+    path:"/",
+    element:<h1>hello</h1>
+  }
+])
+root.render(< RouterProvider router={router}/>);
 
