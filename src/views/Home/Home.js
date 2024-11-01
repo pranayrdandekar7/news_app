b5356926ab4573a4720b3f01caa260d3a2bb647c
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2024-11-01 10:15:06 +0530

    some changes made

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -7,14 +7,17 @@ function Home() {
 
     const [news, setNews] = useState([])
     const [searchQuery,setSearchQuery] =useState("pune")
+    console.log(searchQuery)
 
     const loadNews = async () => {
          try{
-        const response = await axios.get(`https://newsapi.org/v2/everything?q=${searchQuery}&from=2024-10-24&to=2024-10-24&sortBy=popularity&apiKey=${process.env.REACT_APP_API_KEY}`)
+        const response = await axios.get(`https://newsapi.org/v2/everything?q=${searchQuery}&from=2024-11-24&to=2024-10-24&sortBy=popularity&apiKey=${process.env.REACT_APP_API_KEY}`)
         setNews(response.data.articles)
+
+        
          }
          catch(err){
-            console.log(err.massage)
+            console.log(err.message)
          }
     }
 
@@ -28,14 +31,15 @@ function Home() {
 
     return (
         <>
-            <h1>News App </h1>
-            <input placeholder='search'
+            <h1 className='main-heading'>News App </h1>
+            <input
+             placeholder='search'
+             className='search-input'
             value={searchQuery}
             onChange={(e)=>
                 setSearchQuery(e.target.value)
             }
             >
-
             </input>
             <div className='news-container'>
               
